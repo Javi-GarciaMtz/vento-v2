cd2a694c51a9e0838b3ff3da018466192baa5e2d
MiltonPonceRodriguez1 <miltonponceipn@gmail.com>
2022-05-03 01:18:57 -0500

    demo-final-II

--- frontend/src/app/app.component.ts
+++ frontend/src/app/app.component.ts
@@ -1,5 +1,5 @@
 import { Component, OnInit } from '@angular/core';
-import { faMotorcycle, faUserLock, faPlusCircle, faBorderAll, faGaugeHigh, faEdit, faTrash, faTags, faListCheck, faBoxArchive, faCartFlatbed, faFilePen, faHomeUser, faHomeAlt, faHomeLg } from '@fortawesome/free-solid-svg-icons';
+import { faMotorcycle, faUserLock, faPlusCircle, faBorderAll, faGaugeHigh, faEdit, faTrash, faTags, faListCheck, faBoxArchive, faCartFlatbed, faFilePen, faHomeUser, faHomeAlt, faHomeLg, faUser, faUserAlt, faUserAltSlash, faUsers, faUsersBetweenLines, faUsersCog, faUsersGear, faUserShield, faUserPlus, faUsersSlash } from '@fortawesome/free-solid-svg-icons';
 import { Router, NavigationEnd } from "@angular/router";
 
 @Component({
@@ -23,6 +23,8 @@ export class AppComponent implements OnInit {
   public faBoxArchive = faBoxArchive;
   public faFilePen = faFilePen;
   public faHomeLg = faHomeLg;
+  public faUserShield = faUsersGear;
+  public faUserPlus = faUserPlus;
 
   ngOnInit() {
     //console.log('Webapp cargada correctamente!!');
